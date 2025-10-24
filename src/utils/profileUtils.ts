1e4fc55e63ee25236eebbee0adcce8e6e901ae16
fkgrkyr <fkgrkyr@gmail.com>
2025-10-24 19:11:38 +0900

    chore: 채팅목록 json 데이터 추가

--- src/utils/profileUtils.ts
+++ src/utils/profileUtils.ts
@@ -5,7 +5,7 @@ import TaerinProfile from '@/assets/svgs/chatroom/taerin-profile.svg';
 import JiminProfile from '@/assets/svgs/chatroom/jimin-profile.svg';
 import UhginProfile from '@/assets/svgs/chatroom/uhgin-profile.svg';
 import DoriProfile from '@/assets/svgs/chatroom/dori-profile.svg';
-import FaterProfile from '@/assets/svgs/chatroom/father-profile.svg';
+import FatherProfile from '@/assets/svgs/chatroom/father-profile.svg';
 import JinhwaProfile from '@/assets/svgs/chatroom/jinhwa-profile.svg';
 
 const profileMap: Record<string, string> = {
@@ -18,7 +18,7 @@ const profileMap: Record<string, string> = {
   'user7': DefaultProfileGreen,
   'user8': DefaultProfileGreen,
   'user9': DefaultProfileGreen,
-  'user10': FaterProfile,
+  'user10': FatherProfile,
   'user11': DoriProfile,
   'user12': DefaultProfileGreen,
   'user13': JinhwaProfile,
