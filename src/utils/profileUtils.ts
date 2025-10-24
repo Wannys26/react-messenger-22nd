ab4fef096c045526ae068b61f9a68311e543bdc5
fkgrkyr <fkgrkyr@gmail.com>
2025-10-24 19:02:09 +0900

    chore: users 데이터 추가

--- src/utils/profileUtils.ts
+++ src/utils/profileUtils.ts
@@ -4,6 +4,9 @@ import DefaultProfileGreen from '@/assets/svgs/chatroom/default-profile-green.sv
 import TaerinProfile from '@/assets/svgs/chatroom/taerin-profile.svg';
 import JiminProfile from '@/assets/svgs/chatroom/jimin-profile.svg';
 import UhginProfile from '@/assets/svgs/chatroom/uhgin-profile.svg';
+import DoriProfile from '@/assets/svgs/chatroom/dori-profile.svg';
+import FaterProfile from '@/assets/svgs/chatroom/father-profile.svg';
+import JinhwaProfile from '@/assets/svgs/chatroom/jinhwa-profile.svg';
 
 const profileMap: Record<string, string> = {
   'user1': MomProfile,
@@ -12,6 +15,13 @@ const profileMap: Record<string, string> = {
   'user4': TaerinProfile,
   'user5': JiminProfile,
   'user6': UhginProfile,
+  'user7': DefaultProfileGreen,
+  'user8': DefaultProfileGreen,
+  'user9': DefaultProfileGreen,
+  'user10': FaterProfile,
+  'user11': DoriProfile,
+  'user12': DefaultProfileGreen,
+  'user13': JinhwaProfile,
 };
 
 export const getProfileImage = (userId: string): string => {
