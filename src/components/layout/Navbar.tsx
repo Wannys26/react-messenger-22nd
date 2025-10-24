e601e0fb9d84e35a874e67bf6b1ffd21210c39d4
fkgrkyr <fkgrkyr@gmail.com>
2025-10-24 19:23:23 +0900

    feat: 안 읽은 메시지 있을시 navbar 대화 아이콘 수정

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,11 @@
 import { useNavigate, useLocation } from 'react-router-dom';
+import { useChatStore } from '@/store/chatStore';
 import Home from '@/assets/svgs/navbar/home.svg';
 import HomeFill from '@/assets/svgs/navbar/home-fill.svg';
 import Chat from '@/assets/svgs/navbar/chat.svg';
 import ChatFill from '@/assets/svgs/navbar/chat-fill.svg';
 import Call from '@/assets/svgs/navbar/call.svg';
+import UnreadIndicator from '@/assets/svgs/navbar/unread-indicator.svg';
 
 interface NavbarProps {
   onCallClick?: () => void;
@@ -12,9 +14,13 @@ interface NavbarProps {
 const Navbar = ({ onCallClick }: NavbarProps) => {
     const navigate = useNavigate();
     const location = useLocation();
+    const { chatRooms } = useChatStore();
 
     const isHome = location.pathname === '/home';
     const isChat = location.pathname === '/' || location.pathname.startsWith('/chatroom');
+
+    // 읽지 않은 메시지가 있는지 확인
+    const hasUnreadMessages = chatRooms.some(room => room.unreadCount > 0);
 
     const handleHomeClick = () => {
         navigate('/home');
@@ -43,7 +49,17 @@ const Navbar = ({ onCallClick }: NavbarProps) => {
                 className='flex flex-col flex-1 justify-center items-center gap-1 align-stretch cursor-pointer'
                 onClick={handleChatClick}
             >
-                <img src={isChat ? ChatFill : Chat} alt='chat' />
+                <div className="relative">
+                    <img src={isChat ? ChatFill : Chat} alt='chat' />
+                    {/* 읽지 않은 메시지가 있을 때 초록색 점 표시 */}
+                    {hasUnreadMessages && (
+                        <img 
+                            src={UnreadIndicator} 
+                            alt='unread' 
+                            className="absolute -top-[2px] -right-[2px] w-3 h-3"
+                        />
+                    )}
+                </div>
                 <div className={`text-body3-m1 ${isChat ? 'text-gray-7' : 'text-gray-6'}`}>채팅</div>
             </div>
             <div 
