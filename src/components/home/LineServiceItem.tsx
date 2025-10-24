ef9adc7635d709f86928abe8d90ade855683ed07
fkgrkyr <fkgrkyr@gmail.com>
2025-10-24 18:13:46 +0900

    fix: 잘못된  주석 수정

--- src/components/home/LineServiceItem.tsx
+++ src/components/home/LineServiceItem.tsx
@@ -2,7 +2,7 @@ const LineServiceItem = () => {
   return (
     <div className="flex items-center justify-between w-[335px] mx-auto py-3">
       <span className="text-body3-m1 text-gray-6">LINE 서비스</span>
-      // arrow-down.svg로 대체 필요
+      {/* arrow-down.svg로 대체 필요 */}
       <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
         <path
           d="M7.5 5L12.5 10L7.5 15"
